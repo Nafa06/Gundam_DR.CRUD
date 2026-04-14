f9786f435f45a16e827cb84a5619a1c97a1eca9e
Nafa06 <naufalanggardi09@gmail.com>
2026-04-14 10:42:08 +0700

    Add project files.

---
+++ CRUDMahasiswaMySQL/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form1 : Form
+    {
+        private readonly MySqlConnection conn;
+        private readonly string connectionString = "Server = localhost; Database = DBAkademikADO; UID = root; Password = 21914113;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connectionString);
+        }
+
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void ClearForm()
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
